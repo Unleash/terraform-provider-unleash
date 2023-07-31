e6b7c908ad143026dfcaa2c3a7517de325e2a733
Gastón Fournier <gaston@getunleash.io>
2023-07-31 15:02:41 +0200

    Lint

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -11,24 +11,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
-const (
-	unleashConfig = `terraform {
-		required_providers {
-			unleash = {
-				source = "Unleash/unleash"
-				version = ">= 0.0.1"
-			}
-		}
-	}
-
-	provider "unleash" {
-		base_url = "http://localhost:4242"
-		authorization = "*:*.unleash-insecure-admin-api-token"
-	}
-	
-	`
-)
-
 // testAccProtoV6ProviderFactories are used to instantiate a provider during
 // acceptance testing. The factory function will be invoked for every Terraform
 // CLI command executed to create a provider server to which the CLI can
@@ -43,7 +25,7 @@ func envOrDefault(name string, defaultValue string) {
 	}
 }
 
-func testAccPreCheck(t *testing.T) {
+func testAccPreCheck(_ *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
