ea48a2c93e47a34a4210afb843347842898afcd2
Gastón Fournier <gaston@getunleash.io>
2023-09-25 12:10:30 +0200

    Add examples

---
+++ examples/data-sources/unleash_role/data-source.tf
@@ -0,0 +1,11 @@
+data "unleash_role" "admin_role" {
+  name = "Admin"
+}
+
+data "unleash_role" "editor_role" {
+  name = "Editor"
+}
+
+data "unleash_role" "project_member_role" {
+  name = "Member"
+}
