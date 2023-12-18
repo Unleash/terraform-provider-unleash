3896b28dcb26b3bc18c58f5b019dd7d757568dc8
Gastón Fournier <gaston@getunleash.io>
2023-12-18 14:58:55 +0100

    docs: include import capabilities

--- examples/resources/unleash_project/resource.tf
+++ examples/resources/unleash_project/resource.tf
@@ -1,3 +1,14 @@
+import {
+  id = "default"
+  to = unleash_project.default_project
+}
+
+resource "unleash_project" "default_project" {
+  id          = "default"
+  name        = "Default project"
+  description = "Default project now managed by Terraform"
+}
+
 resource "unleash_project" "test_project" {
   id          = "my_project"
   name        = "My Terraform project"
