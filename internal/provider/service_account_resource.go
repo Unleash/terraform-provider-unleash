63c2be04941d223145823acd33a5a9b797624f18
Simon Hornby <simon@getunleash.io>
2024-08-29 17:07:43 +0200

    chore: update docs for service accounts and tokens with some examples (#188)
    * chore: update docs for service accounts and tokens with some examples and explanations
    
    ---------
    
    Co-authored-by: Melinda Fekete <melinda.fekete@getunleash.io>

--- internal/provider/service_account_resource.go
+++ internal/provider/service_account_resource.go
@@ -71,6 +71,12 @@ func (r *serviceAccountResource) Schema(_ context.Context, _ resource.SchemaRequ
 				Required:    true,
 			},
 		},
+		MarkdownDescription: `Service accounts do nothing on their own, they need service account tokens to be created to do anything useful.
+		All tokens bound to a service account will take on the permissions of the root role assigned to the service account. It's strongly
+		recommended to use the unleash_role data source to retrieve one of the built-in roles and use the id from that to set the service account
+		role id.
+
+		See the [Unleash documentation](https://docs.getunleash.io/reference/service-accounts) for more information.`,
 	}
 }
 
