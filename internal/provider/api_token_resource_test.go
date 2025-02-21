d2f9a1a84a65be4f1c0fa890189a851eda10848b
Gastón Fournier <gaston@getunleash.io>
2025-02-21 17:01:05 +0100

    chore: add license and remove admin tokens

--- internal/provider/api_token_resource_test.go
+++ internal/provider/api_token_resource_test.go
@@ -81,55 +81,6 @@ func TestAccApiTokenResource(t *testing.T) {
 					resource.TestCheckResourceAttr("unleash_api_token.client_no_expire", "projects.0", "default"),
 				),
 			},
-			{
-				Config: `
-				resource "unleash_api_token" "admin_token" {
-					token_name = "admin_token"
-					type = "admin"
-					expires_at = "2024-12-31T23:59:59Z"
-					projects = ["*"]
-					environment = "*"
-				}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("unleash_api_token.admin_token", "secret"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_token", "expires_at", "2024-12-31T23:59:59Z"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_token", "token_name", "admin_token"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_token", "environment", "*"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_token", "project", "*"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_token", "projects.0", "*"),
-				),
-			},
-			{
-				Config: `
-				resource "unleash_api_token" "admin_no_expire" {
-					token_name = "admin_no_expire"
-					type = "admin"
-				}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("unleash_api_token.admin_no_expire", "secret"),
-					resource.TestCheckNoResourceAttr("unleash_api_token.admin_no_expire", "expires_at"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "token_name", "admin_no_expire"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "environment", "*"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "project", "*"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "projects.0", "*"),
-				),
-			},
-			{ // test add expire date to previous token
-				Config: `
-				resource "unleash_api_token" "admin_no_expire" {
-					token_name = "admin_no_expire"
-					type = "admin"
-					expires_at = "2024-12-31T23:59:59Z"
-				}`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("unleash_api_token.admin_no_expire", "secret"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "expires_at", "2024-12-31T23:59:59Z"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "token_name", "admin_no_expire"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "environment", "*"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "project", "*"),
-					resource.TestCheckResourceAttr("unleash_api_token.admin_no_expire", "projects.0", "*"),
-				),
-			},
 		},
 	})
 }
