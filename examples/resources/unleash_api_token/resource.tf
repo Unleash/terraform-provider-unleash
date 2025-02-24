370d57815e935510971ef9920d1bb88a9a85f349
Gastón Fournier <gaston@getunleash.io>
2025-02-24 09:09:25 +0100

    chore: upgrade deps and remove deprecated linters (#212)
    * chore: upgrade deps and remove deprecated linters
    
    * chore: add license and remove admin tokens
    
    * Add terraform
    
    * Newly generated docs without admin tokens
    
    * No need for a matrix on generate
    
    * Test different unleash versions
    
    * Fix env variable
    
    * Remove additional versions
    
    * Remove leftovers

--- examples/resources/unleash_api_token/resource.tf
+++ examples/resources/unleash_api_token/resource.tf
@@ -13,16 +13,3 @@ resource "unleash_api_token" "frontend_token" {
   projects    = ["*"]
   environment = "development"
 }
-
-resource "unleash_api_token" "admin_token" {
-  token_name  = "admin_token"
-  type        = "admin"
-  expires_at  = "2024-12-31T23:59:59Z"
-  projects    = ["*"]
-  environment = "*"
-}
-
-resource "unleash_api_token" "admin_no_expire" {
-  token_name = "admin_no_expire"
-  type       = "admin"
-}
