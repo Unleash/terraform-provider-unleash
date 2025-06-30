c48d341debdad4e9c24754daaf857b049250c571
Gastón Fournier <gaston@getunleash.io>
2025-06-30 14:50:57 +0200

    chore: remove example so it's less relevant

--- examples/resources/unleash_api_token/resource.tf
+++ examples/resources/unleash_api_token/resource.tf
@@ -1,11 +1,3 @@
-resource "unleash_api_token" "client_token" {
-  token_name  = "client_token"
-  type        = "client"
-  expires_at  = "2024-12-31T23:59:59Z"
-  project     = "default"
-  environment = "development"
-}
-
 resource "unleash_api_token" "frontend_token" {
   token_name  = "frontend_token"
   type        = "frontend"
